0085ec917bea1ddd2b3b57b3e95841138341b84c
antevg <antevg@gmail.com>
2024-03-26 15:21:46 +0300

    Открывается окно итема

--- composeApp/src/commonMain/kotlin/com/eacipher/shopapp/fav_screen/ItemUiPreview.kt
+++ composeApp/src/commonMain/kotlin/com/eacipher/shopapp/fav_screen/ItemUiPreview.kt
@@ -10,12 +10,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DeleteSweep
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,7 +24,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.eacipher.shopapp.shopping_list_screen.ShoppingListEvent
+
 
 
 @Composable
@@ -48,26 +46,7 @@ fun ItemUiPreview(item: Fav, onEvent: (FavEvents) -> Unit) {
                 }
         ) {
             Column {
-                Text(
-                    text = item.name,
-                    style = TextStyle(
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 16.sp
-                    )
-                )
-                //Time
-                Text(
-                    text = item.date_add ?: "",
-                    style = TextStyle(
-                        color = Color.Gray,
-                        fontSize = 12.sp
-                    ),
-                    modifier = Modifier.padding(top = 5.dp)
-                )
-            }
-            Column {
-                //Image
+                //Photo
                 IconButton(
                     onClick = {
                         onEvent(FavEvents.OnItemClick(item))
@@ -86,7 +65,28 @@ fun ItemUiPreview(item: Fav, onEvent: (FavEvents) -> Unit) {
                         tint = Color.White
                     )
                 }
+
+
+                Text(
+                    text = item.name,
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    ),
+                    maxLines = 2
+                )
+                //Time
+                Text(
+                    text = item.date_add ?: "",
+                    style = TextStyle(
+                        color = Color.Gray,
+                        fontSize = 12.sp
+                    ),
+                    modifier = Modifier.padding(top = 5.dp)
+                )
             }
+
         }
     }
 
